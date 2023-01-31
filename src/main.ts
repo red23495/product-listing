d236d037ead3ba70b6dfac789970d16c7d793964
red23495 <mamun.rashid.hridoy@gmail.com>
2023-01-31 23:29:03 +0600

    typo fixed

--- src/main.ts
+++ src/main.ts
@@ -12,10 +12,10 @@ async function bootstrap() {
   await prismaService.enableShutdownHooks(app);
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Product Crud example')
+    .setDescription('Product Crud API description')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('product')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
